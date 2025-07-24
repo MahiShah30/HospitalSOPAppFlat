55db245c8c1f4ba2da363ccd4c63a3e450d17a91
Mahi Shah <mahi30shah@gmail.com>
2025-07-24 22:10:50 +0530

    Inital commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDXiZZ4rGg8C7YNQ_aG4cJzwIWdSUFvCh0",
   authDomain: "hospitalsop-ecf92.firebaseapp.com",
   projectId: "hospitalsop-ecf92",
   storageBucket: "hospitalsop-ecf92.firebasestorage.app",
